03cd95e8a821ca8d6d08c39c985463a82edcba6f
vrcode <vilasayrathahao@gmail.com>
2021-07-09 12:54:31 -0500

    cmm

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/portfolio-react",
   siteMetadata: {
     title: `Development`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
@@ -33,4 +34,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
