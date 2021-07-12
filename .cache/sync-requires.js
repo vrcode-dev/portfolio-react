ac06233b923ee303c92b67730e7a7ae9e34aef96
vrcode <vilasayrathahao@gmail.com>
2021-07-12 12:20:57 -0500

    commit

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/vilasayrathahao/Documents/github/portfolio-react/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/vilasayrathahao/Documents/github/portfolio-react/src/pages/404.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/Users/vilasayrathahao/Documents/github/portfolio-react/src/pages/about.js"))),
   "component---src-pages-contact-js": hot(preferDefault(require("/Users/vilasayrathahao/Documents/github/portfolio-react/src/pages/contact.js"))),
